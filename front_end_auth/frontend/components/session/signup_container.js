7ff3085ab5eabecb785526d82fe51605ec6c8fd7
Kenneth Choi <kennethichoi@gmail.com>
2019-06-28 10:12:53 -0700

    Initial commit w/ phase 1-5 completed

---
+++ front_end_auth/frontend/components/session/signup_container.js
@@ -0,0 +1,10 @@
+
+import { connect } from 'react-redux';
+import { createNewUser } from '../../actions/sessions';
+import Signup from './signup';
+
+const mapDispatchToProps = dispatch => ({
+  createNewUser: formUser => dispatch(createNewUser(formUser))
+});
+
+export default connect(null, mapDispatchToProps)(SignUp);
